c8d75e71579174481df68546a1c6b9aa33005e28
Luis Paulin <lfpaulin@gmail.com>
2021-10-11 11:22:11 -0700

    add "-Y" param to minimap (soft clipping)

--- simulatie-data/run-minimap2.sh
+++ simulatie-data/run-minimap2.sh
@@ -3,6 +3,7 @@
 # -a:   out is SAM
 # -x:   preset of data source (ont, pacbio)
 # -t:   number of threads to use (def = 3)
+# -Y:   use soft clipping for supp. alignments
 # --MD: add MD tag
 
 REFERENCE=$1
@@ -11,7 +12,8 @@ SORTED_BAM_OUT=$3
 THREADS=$4
 
 minimap2  -ax map-ont \
-    "${REFERENCE}"
-    "${READS}"
-    "--MD" -t "${THREADS}" | \
+    "${REFERENCE}" \
+    "${READS}" \
+    "--MD" -t "${THREADS}" \
+    -Y | \
 samtools sort -@ ${THREADS} - > "${SORTED_BAM_OUT}"
